2cb6be9018ca49b35ff8c1ea2f411d429f929852
Matic-M <26011526+Matic-M@users.noreply.github.com.>
2024-10-18 21:07:42 +0200

    added fair dispatching

--- distributing_tasks/worker.py
+++ distributing_tasks/worker.py
@@ -8,6 +8,7 @@ import sys
 # 1 task + 2 workers
 # each worker gets the same number of messages - round robin
 # added FEATURE for ack after worker finishes, in case it dies message is handed to other worker
+# NEW FEATURE: in case node crashes, queue is not lost -> durable
 
 def main():
      # establish connextion with rabbitmq server
@@ -15,7 +16,7 @@ def main():
     connection = pika.BlockingConnection(pika.ConnectionParameters('localhost', credentials=credentials))
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello')
+    channel.queue_declare(queue='hello_durable', durable=True)
 
     def callback(ch, method, properties, body):
         print(f" [x] Received {body.decode()}")
@@ -23,7 +24,8 @@ def main():
         print(" [x] Done")
         ch.basic_ack(delivery_tag = method.delivery_tag)
 
-    channel.basic_consume(queue='hello', on_message_callback=callback) # remove auto_ack=True arg
+    channel.basic_qos(prefetch_count=1) # added fair dispatch so dispatcher gives one message to worker at the time
+    channel.basic_consume(queue='hello_durable', on_message_callback=callback) # remove auto_ack=True arg
 
     print(' [*] Waiting for messages. To exit press CTRL+C')
     channel.start_consuming()
